375eea85c1b8a6d10b3c3bfa40846aa36941b62a
mitsuyoshi <m123matsuo@icloud.com>
2020-04-15 20:12:03 +0900

    Installing cocoapods and setting up the firebase

---
+++ iCoffee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iCoffee
+//
+//  Created by mitsuyoshi matsuo on 2020/04/15.
+//  Copyright © 2020 mitsuyoshi matsuo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
